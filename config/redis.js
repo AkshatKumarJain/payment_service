ff4b89c179db26513241396e3958410269c4a0bc
Akshat Jain <gomat2230@gmail.com>
2025-11-02 00:30:45 +0530

    debug code: correct redis_port

--- config/redis.js
+++ config/redis.js
@@ -1,4 +1,4 @@
-import redis from "ioredis"
+import Redis from "ioredis"
 
 const redis = new Redis({
     host: process.env.REDIS_HOST,
--- routes/payments.routes.js
+++ routes/payments.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { authMiddleware } from "redis-jwt-auth";
-import { createIntent, confirms } from "../controllers/payments.controller.js";
+import { createIntent, confirm } from "../controllers/payments.controller.js";
 
 const router = express.Router();
 
 router.post('/intent', authMiddleware({ require: true }), createIntent);
-router.post('/confirm', authMiddleware({ require: true }), confirms);
+router.post('/confirm', authMiddleware({ require: true }), confirm);
 
 export default router;
